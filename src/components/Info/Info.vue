87ff2aa8faef1d7e6568fc0f11428f6dfb51667a
lauracarlotta <carlotta.custodio@gmail.com>
2022-06-03 18:10:31 -0300

    WIP

--- src/components/Info/Info.vue
+++ src/components/Info/Info.vue
@@ -11,6 +11,11 @@
 		<p v-if="4 > 2">Teste 2</p>
 
 		<p v-show="showEmail">Mande um email para: {{ email }} </p> <!-- Mostra ou não mostra, não há um 'v-show-else'. Geralmente usado para botões checkbox -->
+		
+		<p v-show="showPhone">Mande um email para: {{ phone }} </p>
+		<div>
+			<button @click="phoneNumber">Mostra Telefone</button>
+		</div>
 
 		<p>Para acessar o meu portifolio, <a v-bind:href="link" target="blank" rel="noreferrer noopener">basta clicar aqui</a></p>
 	</div>
@@ -30,7 +35,9 @@
 			return {
 				isWorking: false,
 				showEmail: true,
+				showPhone: true,
 				email: 'laura@gmail.com',
+				phone: '(11) 98758-8455',
 				link: 'https://www.linkedin.com/in/lauracarlotta/'
 			}
 		},
