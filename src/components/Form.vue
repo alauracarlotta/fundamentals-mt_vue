fed63a78e3c2bd559e9567fc3e799aae81119f61
lauracarlotta <carlotta.custodio@gmail.com>
2022-06-03 20:05:56 -0300

    v-model

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,18 +1,55 @@
 <template>
-	<InputText/>
-	<InputText/>
-	<Submit/>
+	<div>
+		<form @submit="sendForm($event)">
+			<!-- <InputText/> -->
+			<!-- <InputText/> -->
+			<br/>
+			<div>
+				Apelido: 
+				<input type="text" v-model="apelido">
+			</div>
+			<div>
+				Cor Favorita: 
+				<input type="text" v-model="corFavorita">
+			</div>
+			<br/>
+			<Submit/>
+		</form>
+	</div>
 </template>
 
 <script>
-	import InputText from './Form/InputText.vue'
+	// import InputText from './Form/InputText.vue'
 	import Submit from './Form/Submit.vue'
 	export default {
 		name: 'Form',
 
 		components: {
-			InputText,
+			// InputText,
 			Submit,
+		},
+
+		data() {
+			return {
+				apelido: "",
+				corFavorita: "",
+			}
+		},
+
+		methods: {
+			sendForm(e) {
+				e.preventDefault();
+				
+				const apelido = this.apelido;
+				const corFavorita = this.corFavorita;
+
+				console.log('apelido', apelido);
+				console.log('cor', corFavorita);
+
+				// AJAX
+
+				// Inserir nop banco, os dados
+			}
 		}
 	}
 </script>
