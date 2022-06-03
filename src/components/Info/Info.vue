f06d9a7e56abbab39e2512eaba073fa0ad9c86cc
lauracarlotta <carlotta.custodio@gmail.com>
2022-06-03 15:06:08 -0300

    v-bind

--- src/components/Info/Info.vue
+++ src/components/Info/Info.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
+		<Picture/>
 		<p v-if="isWorking">Estou trabalhando no momento;</p>
 		<p v-else>Estou em busca de novas oportunidades, posso iniciar imediatamente;</p>
 		<p>Tenho a expriência de 1 ano como dev front-end;</p>
@@ -10,18 +11,27 @@
 		<p v-if="4 > 2">Teste 2</p>
 
 		<p v-show="showEmail">Mande um email para: {{ email }} </p> <!-- Mostra ou não mostra, não há um 'v-show-else'. Geralmente usado para botões checkbox -->
+
+		<p>Para acessar o meu portifolio, <a v-bind:href="link" target="blank" rel="noreferrer noopener">basta clicar aqui</a></p>
 	</div>
 </template>
 
 <script>
+	import Picture from '../Picture/Picture.vue';
+
 	export default {
 		name: 'Info',
+
+		components: {
+			Picture,
+		},
 
 		data() {
 			return {
 				isWorking: false,
 				showEmail: true,
-				email: 'laura@gmail.com'
+				email: 'laura@gmail.com',
+				link: 'https://www.linkedin.com/in/lauracarlotta/'
 			}
 		},
 	}
---
+++ src/components/Picture/Picture.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="picturesProfile">
+		<img :src="imageLinguage" :alt="altImageLanguage" class="bgc-image">
+		<img :src="avatar" :alt="altAvatar" class="profile-image">
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Picture',
+
+		data() {
+			return {
+				imageLinguage: '/image/js.jpg',
+				altImageLanguage: 'Javascript language on picture with backgroun in black and other colors',
+				avatar: 'https://i.ibb.co/2khHMTS/135861044-3663530760381961-1838278676959512214-n.jpg',
+				altAvatar: 'Profile Image',
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped src="./Picture.scss" />
