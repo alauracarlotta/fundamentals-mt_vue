67d1811be9f54d1e06ab94bfc7c0cfbdc78ae3ac
lauracarlotta <carlotta.custodio@gmail.com>
2022-06-03 13:17:22 -0300

    V-if, v-else and v-show

--- src/components/Info/Info.vue
+++ src/components/Info/Info.vue
@@ -1,14 +1,28 @@
 <template>
 	<div>
-		<p>Não estou trabalhando no momento;</p>
+		<p v-if="isWorking">Estou trabalhando no momento;</p>
+		<p v-else>Estou em busca de novas oportunidades, posso iniciar imediatamente;</p>
 		<p>Tenho a expriência de 1 ano como dev front-end;</p>
 		<p>Trabalhei com liguagens: JS, HTM e CSS;</p>
 		<p>Usei como ferramentas também: Linux ubuntu e git.</p>
+
+		<p v-if="1 > 2">Teste 1</p>
+		<p v-if="4 > 2">Teste 2</p>
+
+		<p v-show="showEmail">Mande um email para: {{ email }} </p> <!-- Mostra ou não mostra, não há um 'v-show-else'. Geralmente usado para botões checkbox -->
 	</div>
 </template>
 
 <script>
 	export default {
 		name: 'Info',
+
+		data() {
+			return {
+				isWorking: false,
+				showEmail: true,
+				email: 'laura@gmail.com'
+			}
+		},
 	}
 </script>
